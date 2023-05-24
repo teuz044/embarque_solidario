5280e22f80587f6ae4ddaff0466a7dcc80006855
Matheus Gabriel <103612213+teuz044@users.noreply.github.com>
2023-05-24 18:33:16 -0300

    Delete backend_embarque_solidario directory

--- backend_embarque_solidario/test/backend_embarque_solidario_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:backend_embarque_solidario/backend_embarque_solidario.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
